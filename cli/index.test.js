f90a75473a0b75ac2b35f8ed385a16bb0d5e494b
RafaDw <www.rafadraw@gmail.com>
2020-11-27 16:03:16 -0300

    Update

---
+++ cli/index.test.js
@@ -0,0 +1,13 @@
+
+const { join } = require('path');
+const assert = require('assert');
+
+const { describe, it, before, beforeEach, afterEach } = require('mocha');
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+describe('Suite de manipulação de Heróis', () => {
+	it('Deve Cadastrar um herói, usando arquivos', async () => {
+
+	})
+})
